e76f1b80d05d126ff4833d28f75f9e27e90d275f
Tamara Mack <tamaramack0508@gmail.com>
2019-10-17 02:17:15 -0700

    build correction

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,16 +4,20 @@
 module.exports = {
   root: true,
   env: {
+    es6: true,
+    browser: true,
     node: true
   },
-  'extends': [
+  extends: [
     'eslint:recommended'
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-unused-vars': 0
   },
   parserOptions: {
+    ecmaVersion: 2018,
     parser: 'babel-eslint'
   },
   overrides: [
@@ -26,4 +30,4 @@ module.exports = {
       }
     }
   ]
-}
+};
--- docs/README.md
+++ docs/README.md
@@ -1,8 +1,8 @@
 # T Mack's Portfolio API *(0.0.2)*
 *This project is a **Work in Progress***
 
-**Last Update: 10/17/2019, 01:53:41**
+**Last Update: 10/17/2019, 02:17:17**
 
-**Current Tag: v0.0.2-1-gc99299b**
+**Current Tag: v0.0.2-2-g628250b**
 
 ## [tamaramack/portfolio-api](https://github.com/tamaramack/portfolio-api)
