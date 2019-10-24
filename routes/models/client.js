14c75f84341a2d98d2979ddc5a29af2e951b59e9
Tamara Mack <tamaramack0508@gmail.com>
2019-10-24 10:21:49 -0700

    minor directory fix

--- routes/models/client.js
+++ routes/models/client.js
@@ -1,19 +1,13 @@
 /**
  * clients js file created by Tamara G. Mack on 22-Oct-19 for portfolio-api
  */
-/**
- * blog js file created by Tamara G. Mack on 18-Oct-19 for portfolio-api
- */
 module.exports = ((mongoose) => {
   const Schema = mongoose.Schema;
   const clientSchema = new Schema({
     ip: {type: String},
     users: [{type: Schema.Types.ObjectId, ref: 'Users'}]
   }, {
-    timestamps: {
-      createdAt: 'timestamp',
-      updatedAt: 'updated'
-    }
+    timestamps: true
   });
 
   return mongoose.model('Clients', clientSchema);
